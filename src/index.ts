a21c41bffac5f286870ea8e54452177d498188bd
Rafael Dourado <rafael@sobit.com.br>
2022-10-27 15:04:16 -0300

    segundo commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('typescript')
+import express from 'express'
+
+const server = express()
+
+server.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+
+server.listen(3333, () => {
+    console.log('Server started on port 3333')
+})
+
+
+
